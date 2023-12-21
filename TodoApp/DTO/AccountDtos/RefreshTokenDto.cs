4123ab84361a621560dd3a4dac3ae5d4a74ba6f6
YusifHuseynzade <110456474+YusifHuseynzade@users.noreply.github.com>
2023-12-21 11:26:40 +0400

    Final_Version

--- TodoApp/DTO/AccountDtos/RefreshTokenDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DTO.AccountDtos
-{
-    public class RefreshTokenDto
-    {
-        public string Token { get; set; } = string.Empty;
-        public DateTime Created { get; set; } = DateTime.Now;
-        public DateTime Expires { get; set; }
-    }
-}
